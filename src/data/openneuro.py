15bddaab5ddd20b69563271ac90aaf6cb103639a
Morteza Ansarinia <ansarinia@me.com>
2023-10-22 11:50:50 +0200

    a mock multimodal openneuro data module and fix spatiotemporal autoencoder

--- src/data/openneuro.py
+++ src/data/openneuro.py
@@ -1,20 +1,46 @@
+from typing import Literal
+import torch
 import pytorch_lightning as pl
 
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 from torch.utils.data import random_split
 
 
 class OpenNeuroDataModule(pl.LightningDataModule):
-    def __init__(self, dataset=None, test_size=.2, batch_size=32, shuffle=False):
+    def __init__(self,
+                 modality: Literal['fmri', 'eeg', 'meg'] = 'fmri',
+                 dataset=None,
+                 test_size: float = .2,
+                 batch_size: int = 32,
+                 shuffle: bool = False):
+        """"""
         super().__init__()
-        self.dataset = dataset or self.load_default_dataset()
+        self.modality = modality
+        self.dataset = dataset or OpenNeuroDataModule.load_default_dataset()
+        self.n_subjects = len(self.dataset)
         self.test_size = test_size
         self.batch_size = batch_size
         self.shuffle = shuffle
 
     @classmethod
-    def load_default_dataset(cls):
-        raise NotImplementedError("Please define a default dataset to load.")
+    def load_default_dataset(cls,
+                             modality: Literal['fmri', 'eeg', 'meg'] = 'fmri',
+                             n_subjects=10,
+                             n_timepoints=125,
+                             features_dim=(10, 10, 10)):
+
+        # raise NotImplementedError("Please define a default dataset to load.")
+        # shape: fmri: (n_subjects, n_timepoints, x, y, z)
+        # shape: eeg: (n_subjects, n_timepoints, n_channels)
+        # shape: meg: (n_subjects, n_timepoints, n_channels)
+        data = torch.randn(n_subjects, n_timepoints, *features_dim)
+        subject_labels = torch.arange(0, n_subjects)    # shape: (n_subjects,)
+        labels = torch.randint(0, 2, (n_subjects,))     # shape: (n_subjects,)
+        dataset = TensorDataset(data, subject_labels, labels)
+        return dataset
+
+    def get_example_batch(self):
+        return self.dataset[0:1]  # return first sample
 
     def setup(self, stage=None):
         # Assign train/val datasets for use in dataloaders
--- src/models/spatiotemporal_auto_encoder.py
+++ src/models/spatiotemporal_auto_encoder.py
@@ -4,22 +4,27 @@ from torch import nn
 
 
 class SpatioTemporalAutoEncoder(nn.Module):
-    def __init__(self, n_inputs, n_spatial_features, n_temporal_features):
+    def __init__(self, n_spatial_features, n_temporal_features, example_input_array):
         super().__init__()
         # self.example_input_array = torch.Tensor(32, 124, 32)
         self.n_spatial_features = n_spatial_features
         self.n_temporal_features = n_temporal_features
 
+        # skip batch and time dimensions
+        example_input = example_input_array[0]  # select input, shape: (n_timepoints, *n_features)
+        n_inputs = torch.prod(torch.tensor(example_input.shape[2:])).item()
+
         self.spatial_encoder = nn.Sequential(
             nn.Conv1d(n_inputs, n_spatial_features * 2, 1),
             nn.ReLU(),
             nn.Dropout(0.2),
-            # nn.ReLU(),
             nn.Conv1d(n_spatial_features * 2, n_spatial_features, 1),
         )
 
         self.spatial_decoder = nn.Sequential(
-            nn.ConvTranspose1d(n_spatial_features, n_inputs, kernel_size=3, padding=1),
+            nn.ConvTranspose1d(n_spatial_features, n_spatial_features * 2, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose1d(n_spatial_features * 2, n_inputs, kernel_size=3, padding=1),
         )
 
         self.temporal_encoder = nn.LSTM(n_spatial_features, n_temporal_features, batch_first=True)
@@ -30,22 +35,22 @@ class SpatioTemporalAutoEncoder(nn.Module):
         batch_size = x.size(0)
         n_timepoints = x.size(1)
 
-        # spatial encoding
-        x_s = self.spatial_encoder(x.permute(0, 2, 1)).permute(0, 2, 1)
+        # flatten features
+        x_flatten = x.view(batch_size, n_timepoints, -1)
 
-        # TODO: flatten
+        # spatial encoding
+        y_s_enc = self.spatial_encoder(x_flatten.permute(0, 2, 1)).permute(0, 2, 1)
 
         # temporal encoding
-        y_t_enc, (h_t, c_t) = self.temporal_encoder(x_s)
-        x_t = torch.rand(batch_size, n_timepoints, self.n_temporal_features, device=h_t.device)
+        y_t_enc, (h, c_t) = self.temporal_encoder(y_s_enc)
+        h_enc = h.permute(1, 0, 2).repeat(1, n_timepoints, 1)
 
         # temporal decoding
-        y_t_dec, (h_t, c_t) = self.temporal_decoder(x_t, (h_t, c_t))
-        h = h_t[-1, :, :]  # last hidden state of encoder
+        y_t_dec, (_, _) = self.temporal_decoder(h_enc)
 
-        # TODO deflatten
+        # space decoding
+        y_s_dec = self.spatial_decoder(y_t_dec.permute(0, 2, 1)).permute(0, 2, 1)
 
-        # spatial decoding
-        x_recon = self.spatial_decoder(y_t_dec.permute(0, 2, 1))
+        x_recon = y_s_dec.view(*x.shape)
 
         return x_recon, h
